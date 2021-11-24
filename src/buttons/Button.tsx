a28122ea32d6f9a321c7f1abfa5a230b2423b7e6
dPacc <themdk01@gmail.com>
2021-11-24 17:50:29 +0400

    add: button interface and props

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'small' | 'medium' | 'large';
+  disabled?: boolean;
+  isFullWidth?: boolean;
+}
+
 const Button = React.forwardRef<
   HTMLButtonElement,
-  React.ComponentPropsWithoutRef<'button'>
->(({ children, ...props }, ref) => {
+  ButtonProps
+>(({ children, variant, size, isFullWidth, ...props }, ref) => {
   return (
     <>
-      <button ref={ref} type="button" {...props}>{children}</button>
+      <button ref={ref} type="button" {...props}>
+        {children}
+      </button>
     </>
   );
 });
+
+Button.defaultProps = {
+  variant: 'secondary',
+  size: 'medium',
+  disabled: false,
+  isFullWidth: false,
+};
 
 Button.displayName = 'Button';
 
