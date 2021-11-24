7e10fc9210cb25ad40dc1536b905f5126e58c641
dPacc <themdk01@gmail.com>
2021-11-24 13:41:56 +0400

    add: prettier formatting across the codebase

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import Button  from '../buttons/Button';
+import Button from '../buttons/Button';
 
 export default {
   title: 'Example/Button',
   component: Button,
 } as Meta;
 
-const Template: Story = (args) => (
-  <Button {...args} />
-);
+const Template: Story = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
-
