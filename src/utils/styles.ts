e361e7de6ccf53860c703f7a2bac1c763f379034
dPacc <themdk01@gmail.com>
2021-11-24 18:06:58 +0400

    add: styles const values

---
+++ src/utils/styles.ts
@@ -0,0 +1,7 @@
+export const PRIMARY_COLOR = '#0a58ca';
+export const SECONDARY_COLOR = '#0a58ca';
+export const PRIMARY_TEXT_COLOR = '#fff';
+export const SECONDARY_TEXT_COLOR = '#000';
+export const DANGER_COLOR = '#ff0000';
+export const DANGER_TEXT_COLOR = '#fff';
+export const DISABLED_OPACITY = 0.65;
